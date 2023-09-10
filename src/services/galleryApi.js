c2d000812d36f33710805cbe86d36846724893e5
Andrii <123685813+Andrii1308@users.noreply.github.com>
2023-09-10 21:36:50 +0300

    Delete src directory

--- src/services/galleryApi.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export const fetchPictures = async (query, page) => {
-  const BASE_URL = 'https://pixabay.com/api';
-  const OPTIONS = `q=${query}&page=${page}&key=38417467-04ef37b099f8d5c81e85a4849&image_type=photo&orientation=horizontal&per_page=12`;
-  const response = await axios.get(`${BASE_URL}/?${OPTIONS}`);
-  return response.data;
-};
