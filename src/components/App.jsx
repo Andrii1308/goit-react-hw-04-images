107635a34e5a2b6beb8ca0bd4526664837a164e1
Andrii1308 <123685813+Andrii1308@users.noreply.github.com>
2023-09-10 21:20:59 +0300

    ready

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,62 +1,70 @@
 import { useEffect, useState } from 'react';
-import { fetchImages } from 'api';
-import { Button } from './Button/Button';
-import { ImageGallery } from './ImageGallery/ImageGallery';
-import { Layout } from './Layout';
-import { Loader } from './Loader/Loader';
-import { SearchBar } from './Searchbar/SearchBar';
+import { fetchImages } from "api";
+import { Button } from "./Button/Button";
+import { ImageGallery } from "./ImageGallery/ImageGallery";
+import { Layout } from "./Layout";
+import { Loader } from "./Loader/Loader";
+import { SearchBar } from "./Searchbar/SearchBar";
 import { toast, Toaster } from 'react-hot-toast';
-import { ModalComponent } from './Modal/Modal';
+import { ModalComponent } from "./Modal/Modal";
 
-export const App = () => {
-  const [query, setQuery] = useState('');
-  const [images, setImages] = useState([]);
-  const [page, setPage] = useState(1);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
-  const [loadMore, setLoadMore] = useState(false);
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [largeImageURL, setLargeImageURL] = useState('');
-  const [tag, setTag] = useState('');
-  const onSearch = evt => {
-    evt.preventDefault();
-    const value = evt.target.query.value.trim('');
-    if (value === '') return toast.error(`Enter your request`);
-    setQuery(`${Date.now()}/${value}`);
-    setImages([]);
-    setPage(1);
-  };
+
+export const App =() => {
+
+  const [query, setQuery] = useState('')
+  const [images, setImages] = useState([])
+  const [page, setPage] = useState(1)
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(false)
+  const [loadMore, setLoadMore] = useState(false)
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const [largeImageURL, setLargeImageURL] = useState('')
+  const [tag, setTag] = useState('')
+
+  const onSearch = (evt) => {
+     evt.preventDefault();
+     const value = evt.target.query.value.trim('');
+    if (value === '') return toast.error(`Enter your request`);  
+    setQuery(`${Date.now()}/${value}`)
+    setImages([])
+    setPage(1)
+  } 
 
   const handlerLoadMore = () => {
     setPage(page + 1);
-  };
+  }
 
   useEffect(() => {
     if (!query) return;
-    async function searchImage() {
-      try {
+  async function searchImage() { 
+        try {
         setLoading(true);
         setError(false);
-        const { hits, totalHits } = await fetchImages(query.slice(14), page);
-        if (hits.length === 0) {
-          return toast.error(`We didn't find anything. Try again`);
-        } else {
-          if (page === 1) {
-            toast.success(`We find ${totalHits} pictures`);
-          }
-        }
-        setImages(prevState => [...prevState, ...hits]);
-        setLoadMore(page < Math.ceil(totalHits / 12));
-      } catch (error) {
-        setError(true);
-        toast.error(`OOPS! THERE WAS AN ERROR!`);
-      } finally {
-        setLoading(false);
-      }
+         const { hits, totalHits } = await fetchImages(
+           query.slice(14),
+           page
+         );
+         if (hits.length === 0) {
+           return toast.error(`We didn't find anything. Try again`);
+         } else {
+           if (page === 1) {
+             toast.success(`We find ${totalHits} pictures`);
+           }
+         }
+         setImages((prevState) => [...prevState, ...hits]);;
+          setLoadMore(page < Math.ceil(totalHits / 12))
+       } catch (error) {
+         setError(true);
+         toast.error(`OOPS! THERE WAS AN ERROR!`)
+       }
+       finally {
+         setLoading(false);
+       }
     }
-    searchImage();
-  }, [query, page]);
-
+    searchImage()
+}, [query, page])
+  
+  
   const openModal = (largeImageURL, tag) => {
     setIsModalOpen(true);
     setLargeImageURL(largeImageURL);
@@ -67,26 +75,22 @@ export const App = () => {
     setIsModalOpen(false);
     setLargeImageURL('');
     setTag('');
-  };
+  }
 
-  return (
-    <Layout>
-      <SearchBar onSubmit={onSearch} />
-      {loading && <Loader />}
-      {error && !loading && <div>OOPS! THERE WAS AN ERROR!</div>}
-      {images.length > 0 && (
-        <ImageGallery gallery={images} onImageClick={openModal} />
-      )}
-      {loadMore && <Button onLoadMore={handlerLoadMore} />}
-      {isModalOpen && (
-        <ModalComponent
+    return (
+      <Layout>
+        <SearchBar onSubmit={onSearch} />
+        {loading && <Loader/>}
+        {error && !loading && <div>OOPS! THERE WAS AN ERROR!</div>}
+        {images.length > 0 && <ImageGallery gallery={images} onImageClick={openModal} />}
+        {loadMore && <Button onLoadMore={handlerLoadMore} />}
+        {isModalOpen && <ModalComponent
           isOpen={isModalOpen}
           onRequestClose={closeModal}
           largeImageURL={largeImageURL}
           tag={tag}
-        />
-      )}
-      <Toaster position="top-right" />
-    </Layout>
-  );
-};
+        />}
+        <Toaster position="top-right"/>
+      </Layout>
+    )
+  }
